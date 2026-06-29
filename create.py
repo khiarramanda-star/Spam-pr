3a32a5e8dac9995e9a532498cadb6389f2320047
khiarramanda-star <khiarramanda@gmail.com>
2026-06-29 13:31:29 +0700

    Delete create.py

--- create.py
+++
@@ -1,9 +0,0 @@
-from db_cloud import UserManager
-import platform
-
-username = input("username: ")
-password = input("password: ")
-device_id = platform.node()
-
-success, msg = UserManager.register(username, password, device_id, "ADMINGANTENGBGT")
-print(msg)
